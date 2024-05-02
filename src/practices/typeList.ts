4ed7e97a3a2b81846842f222099f4b195ea12292
Renta-Hoyoshi <renta.lsb.soccer@gmail.com>
2024-05-02 13:29:03 +0900

    React×TypeScriptのチュートリアル

---
+++ src/practices/typeList.ts
@@ -0,0 +1,36 @@
+export {};
+/**TypeScriptの基本の型 */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "A";
+
+// Array
+let arr1: Array<number> = [1, 2, 3];
+let arr2: number[] = [4, 5, 6];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+    const test = "TEST";
+}
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number, name: string } = { id: 0, name: "AAA" };
